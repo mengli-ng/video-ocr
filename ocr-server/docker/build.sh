32382a57d7b25df5670e3bc79983c21c8337ba2f
mengli <mengli@mobike.com>
2017-11-16 15:32:29 +0800

    update docker image

--- ocr-server/docker/build.sh
+++ ocr-server/docker/build.sh
@@ -3,5 +3,5 @@
 dir=`cd $(dirname $0) ; pwd -P`
 
 docker build -t mengli/video-ocr-server/tesseract-ocr "$dir"
-docker tag mengli/video-ocr-server/tesseract-ocr registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server/tesseract-ocr
-docker push registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server/tesseract-ocr
+docker tag mengli/video-ocr-server/tesseract-ocr registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server_tesseract-ocr
+docker push registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server_tesseract-ocr
--- web-console/docker/build.sh
+++ web-console/docker/build.sh
@@ -2,6 +2,6 @@
 
 dir=`cd $(dirname $0) ; pwd -P`
 
-docker build -t dreamcoder/video-ocr-web "$dir"
-docker tag dreamcoder/video-ocr-web:latest registry.cn-beijing.aliyuncs.com/dreamcoder/video-ocr-web:latest
-docker push registry.cn-beijing.aliyuncs.com/dreamcoder/video-ocr-web:latest
+docker build -t mengli/video-ocr-web/tesseract-ocr "$dir"
+docker tag mengli/video-ocr-web/tesseract-ocr registry.cn-beijing.aliyuncs.com/mengli/video-ocr-web_tesseract-ocr
+docker push registry.cn-beijing.aliyuncs.com/mengli/video-ocr-web_tesseract-ocr
