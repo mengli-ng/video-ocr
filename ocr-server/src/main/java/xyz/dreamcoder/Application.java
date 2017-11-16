17099905a0aee0744ad213576ac2515d3f0eef8f
mengli <mengli@mobike.com>
2017-11-16 15:41:15 +0800

    remove activation

--- ocr-server/src/main/java/xyz/dreamcoder/Application.java
+++ ocr-server/src/main/java/xyz/dreamcoder/Application.java
@@ -1,29 +1,14 @@
 package xyz.dreamcoder;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import xyz.dreamcoder.service.ActivationService;
 
 @SpringBootApplication
 @EnableAsync
-public class Application implements InitializingBean {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-	private final ActivationService activationService;
-
-	@Autowired
-	public Application(ActivationService activationService) {
-		this.activationService = activationService;
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		activationService.verify();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
