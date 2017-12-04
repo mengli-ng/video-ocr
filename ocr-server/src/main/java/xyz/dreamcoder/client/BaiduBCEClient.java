978eb893f334ed040a82b3e9c49f7ccfc5b57e6b
Meng Li <mengli@mobike.com>
2017-12-04 16:51:16 +0800

    update recently changed

--- ocr-server/src/main/java/xyz/dreamcoder/client/BaiduBCEClient.java
+++ ocr-server/src/main/java/xyz/dreamcoder/client/BaiduBCEClient.java
@@ -1,21 +1,15 @@
 package xyz.dreamcoder.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.hystrix.HystrixCommand;
-import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.cloud.netflix.feign.support.ResponseEntityDecoder;
-import org.springframework.cloud.netflix.feign.support.SpringDecoder;
 import org.springframework.cloud.netflix.feign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -41,19 +35,6 @@ class FormUrlEncodedConfiguration {
 
     @Autowired
     private ObjectFactory<HttpMessageConverters> messageConverters;
-
-    @Bean
-    public Decoder feignDecoder() {
-        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(customObjectMapper());
-        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
-        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
-    }
-
-    public ObjectMapper customObjectMapper(){
-        ObjectMapper objectMapper = new ObjectMapper();
-        //Customize as much as you want
-        return objectMapper;
-    }
 
     @Bean
     public Encoder feignFormEncoder() {
