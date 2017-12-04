fa041cfb160178ed51bc31da5264169f5d0d203e
Meng Li <mengli@mobike.com>
2017-12-04 11:10:41 +0800

    update docker

--- ocr-server/docker/build.sh
+++ ocr-server/docker/build.sh
@@ -2,6 +2,6 @@
 
 dir=`cd $(dirname $0) ; pwd -P`
 
-docker build -t mengli/video-ocr-server/tesseract-ocr "$dir"
-docker tag mengli/video-ocr-server/tesseract-ocr registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server_tesseract-ocr
-docker push registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server_tesseract-ocr
+docker build -t mengli/video-ocr-server/baidu-ocr "$dir"
+docker tag mengli/video-ocr-server/baidu-ocr registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server_baidu-ocr
+docker push registry.cn-beijing.aliyuncs.com/mengli/video-ocr-server_baidu-ocr
