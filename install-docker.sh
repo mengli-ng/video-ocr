322e76f36994f753408c9d40c2a480c08e2c0a8f
Meng Li <mengli@mobike.com>
2018-01-28 13:16:54 +0800

    update

--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,14 @@
-sudo yum install -y yum-utils \
+yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
 
-sudo yum-config-manager \
+yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce
+yum install docker-ce
+systemctl enable docker; systemctl start docker
+
+mkdir -p /etc/docker
+echo '{ "registry-mirrors": ["https://6adape9u.mirror.aliyuncs.com"] }' > /etc/docker/daemon.json
+systemctl daemon-reload; systemctl restart docker
